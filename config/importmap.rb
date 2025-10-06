656028a2f4402b52df349897eaf65210a62e4ef3
Hikari-huk <ghikariao974@gmail.com>
2025-10-06 19:06:07 +0900

    [fix] レイアウトとpackage.jsonを変更

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,2 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application"
-
-# ✅ これに置き換える（Popper同梱のバンドル）
-pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js", preload: true
