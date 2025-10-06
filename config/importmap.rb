5f22139a6104bf70765bf70d307ceb89204eb584
Hikari-huk <ghikariao974@gmail.com>
2025-10-06 15:06:51 +0900

    [add] Bootstrap JSを追加

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+
+# ✅ これに置き換える（Popper同梱のバンドル）
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
